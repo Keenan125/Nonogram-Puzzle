518727658db6d84911f6fe987e86c03d0655c408
Keenan125 <161631338+Keenan125@users.noreply.github.com>
2024-05-07 14:16:48 +0100

    Made changes to ImageBMP
    Added the InputStream and File variables into the public class, i have used inputstream to read through the elephant.bmp file!

--- ImageBMP.java
+++ ImageBMP.java
@@ -1,4 +1,28 @@
-class ImageBMP{
+import java.io.*;
+
+public class ImageBMP{
+
+    public InputStream fileInput;
+    public File newfile;
+
+    public ImageBMP() throws FileNotFoundException{
+
+        fileInput = new FileInputStream("bmp-files/elephant.bmp");
+        newfile = new File("bmp-files/elephant.bmp");
+
+        fileInput = new FileInputStream(newfile);
+
+
+    }
+
+    public int read(int r) throws IOException{
+        
+
+
+        return r;
+    }
+
+
 
     
 }

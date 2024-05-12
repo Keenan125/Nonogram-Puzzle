7804ff01745d9e5bb3eda7d2844d6989eefb5e85
Keenan125 <161631338+Keenan125@users.noreply.github.com>
2024-05-12 22:05:20 +0100

    Outer Border Started
    Repeat the step of creating numOfCells and set them to grey. Then i can add numbers in. This way is easier as when it comes to the actionlistener part i can sort them as an array so whenever user clicks on the tile that has a number it wont change it will stay the same.

--- Nanogram.java
+++ Nanogram.java
@@ -6,22 +6,43 @@ public class Nanogram{
 
     private GridLayout layout;
     private JPanel panel;
+    private NanogramCells[][] cells;
+    private NanogramCells[] numOfCells;
                                                                                                                                                   
 
     public Nanogram(){
     JFrame frame = new JFrame("**Nanograme Puzzle**");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+    
     panel = new JPanel();
-    layout = new GridLayout(15,15);//15x15 Grid of Nanogram
+    layout = new GridLayout(16,16);//Grid of Nanogram (allows numbers on top)
     panel.setLayout(layout);
 
-    for (int i = 0; i < 15 * 15; i++) {
-        NanogramCells cells = new NanogramCells(); // A simple JButton
-        panel.add(cells); // Adding the button to the panel
+    cells = new NanogramCells[16][16];
+
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            NanogramCells cell = new NanogramCells();
+            cells[i][j] = cell; // Store the cell in the array
+            panel.add(cell); // Add the cell to the panel
+        }
     }
 
+    //Set the outer cells with integers in them with different color
+    numOfCells = new NanogramCells[30];
+    for(int i = 0; i < numOfCells.length; i++){
+        numOfCells[i] = new NanogramCells();
+    }
+    
+    NanogramCells numCell1 = numOfCells[0];
+    numOfCells[0] = cells[0][0];
+    numOfCells[0].setBackground(Color.WHITE);
 
+
+    
+    
 
     frame.setContentPane(panel);
     frame.setSize(1500,1500);
--- NanogramCells.java
+++ NanogramCells.java
@@ -7,17 +7,31 @@ import javax.swing.JButton;
 
 public class NanogramCells extends JButton implements ActionListener{
 
+    private boolean isPressed;
+
 
     public NanogramCells(){
 
-        setBackground(Color.WHITE); //Sets cell to start white
+        setBackground(Color.yellow); //Sets cell to start white
         setBorder(BorderFactory.createLineBorder(Color.BLACK)); //Sets border to black
 
        addActionListener(this);
 
+       isPressed = false;
+
     }
 
     public void actionPerformed(ActionEvent e){
+        isPressed = !isPressed; // allows it to return back to white if pressed accidently
+
+        if(isPressed){
+            setBackground(Color.BLACK); //if clicked goes to black
+        }
+        else{
+            setBackground(Color.yellow);
+        }
+
+        
 
     }
 
